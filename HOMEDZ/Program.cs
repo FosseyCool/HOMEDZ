bafbc840eec6384b50852ac2ddfd4a23d29f2832
FosseyCool <Verein33@gmail.com>
2022-02-25 21:57:13 +0300

    Initial commit

---
+++ HOMEDZ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HOMEDZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
